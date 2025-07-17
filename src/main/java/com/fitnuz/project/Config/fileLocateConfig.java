f5e9884c1eb9036e06c98feba37b33d7ff8c3a48
azeem <azeem@ak.com>
2025-07-17 14:14:32 +0530

    fix: filelocate

--- src/main/java/com/fitnuz/project/Config/fileLocateConfig.java
+++ src/main/java/com/fitnuz/project/Config/fileLocateConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class fileLocateConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("images/**").addResourceLocations("file:images/");
+        registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
     }
 }
