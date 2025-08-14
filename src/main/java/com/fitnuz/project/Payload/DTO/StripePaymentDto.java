3053170185bb87f6817608cfae8ee41678935cd5
azeem <azeem@ak.com>
2025-08-14 21:15:56 +0530

    Update StripePaymentDto fields and structure

--- src/main/java/com/fitnuz/project/Payload/DTO/StripePaymentDto.java
+++ src/main/java/com/fitnuz/project/Payload/DTO/StripePaymentDto.java
@@ -1,8 +1,11 @@
 package com.fitnuz.project.Payload.DTO;
 
+import com.fitnuz.project.Model.Address;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
@@ -10,6 +13,27 @@ import lombok.NoArgsConstructor;
 public class StripePaymentDto {
     private Long amount;
     private String currency;
+    private String email;
+    private String name;
+    private Address address;
+    private String description;
+    private Map<String,String> metadata;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Map<String, String> getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Map<String, String> metadata) {
+        this.metadata = metadata;
+    }
 
     public Long getAmount() {
         return amount;
@@ -25,5 +49,29 @@ public class StripePaymentDto {
 
     public void setCurrency(String currency) {
         this.currency = currency;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
